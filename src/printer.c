6a1cb9d94e2035c61787259a2ccc56a41f928be0
Jesse Bouwman <jesse.bouwman@pm.me>
2025-05-30 09:06:49 -0700

    Support colorization of terminal output

---
+++ src/printer.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h> // isatty == is connected to a teletypewriter
+#include "printer.h"
+
+int has_color(void) {
+    const char *term = getenv("TERM");
+    if (!isatty(STDOUT_FILENO) || term == NULL) {
+        return 0;
+    }
+    return strstr(term, "xterm") != NULL;
+}
+
+void print_color(const char *s, Color c) {
+    int color = has_color();
+    if (color)
+        printf("\033[%dm", c + 30);
+    printf("%s", s);
+    if (color)
+        printf("\033[0m");
+}
